915409b644d0aaf0044d66a9e315ad92b680cfb0
DenerThiago21 <denerthi.ibi@gmail.com/>
2022-05-25 19:34:24 -0300

    injecao de dependencias

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course-service";
 
 @Component({
   selector: 'app-course-list',
@@ -10,29 +11,10 @@ export class CourseListComponent implements OnInit{
 
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService) {}
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular Forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 75.99,
-        code: 'XPTO-4269',
-        duration: 120,
-        rating: 4.4,
-        releaseDate: '12/05/2021'
-      },
-      {
-        id: 2,
-        name: 'Angular HTTP',
-        imageUrl: '/assets/images/http.png',
-        price: 100,
-        code: 'JAVL-3996',
-        duration: 145,
-        rating: 4.7,
-        releaseDate: '02/12/2019'
-      }
-    ]
+    this.courses = this.courseService.retriveAll();
   }
 
 }

d9d11ed1cc02009d867531799c0769360a17669a
DenerThiago21 <denerthi.ibi@gmail.com/>
2022-06-11 21:06:48 -0300

    Links com rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Erro404Component } from './erro-404/erro-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -17,20 +18,25 @@ import { Erro404Component } from './erro-404/erro-404.component';
     StarComponent,
     ReplacePipe,
     NavBarComponent,
-    Erro404Component
+    Erro404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
       {
+        path: 'courses',
+        component: CourseListComponent,
+      },
+      {
+        path: 'courses/info/:id',
+        component: CourseInfoComponent
+      },
+      {
         path: '',
         redirectTo: 'courses',
         pathMatch: 'full'
-      },
-      {
-        path: 'courses',
-        component: CourseListComponent,
       },
       {
         path: '**',
